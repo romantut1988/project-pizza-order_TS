b8f1157cc660eb2236facc19d5a0c069af1f1017
romantut1988 <romantut1988@mail.ru>
2022-06-07 02:04:37 +0300

    app interaction with backend with mockap

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,41 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './scss/app.scss';
 import './components/Header';
 import {Header} from "./components/Header";
 import {Categories} from "./components/Categories";
 import {Sort} from "./components/Sort";
 import {PizzaBlock} from "./components/PizzaBlock";
-import pizzas from "./assets/pizzas.json";
 
 function App() {
+    const [items, setItems] = useState([])
+
+    useEffect(() => {
+        fetch('https://629e737c8b939d3dc2820978.mockapi.io/items')
+            .then((res) => res.json())
+            .then((arr) => {
+                setItems(arr);
+            });
+    }, []);
+
     return (
         <div className="wrapper">
             <Header/>
             <div className="content">
                 <div className="container">
                     <div className="content__top">
-                        {/*<Categories/>*/}
+                        <Categories/>
                         <Sort/>
                     </div>
                     <h2 className="content__title">Все пиццы</h2>
                     <div className="content__items">
-                        {pizzas.map((obj) => (
+                        {items.map(({id, imageUrl, price, sizes, title, types}) => (
                             <PizzaBlock
-                                key={obj.id}
-                                title={obj.title}
-                                price={obj.price}
-                                imageUrl={obj.imageUrl}
-                                sizes={obj.sizes}
-                                types={obj.types }
+                                key={id}
+                                title={title}
+                                price={price}
+                                imageUrl={imageUrl}
+                                sizes={sizes}
+                                types={types}
                             />
                         ))}
                     </div>
