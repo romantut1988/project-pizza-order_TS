0dc440e49405d3fe57ec2324b9a28641eeefac21
romantut1988 <romantut1988@mail.ru>
2022-09-04 23:59:16 +0300

    restart

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import './index.scss';
+
+function App() {
+
+  return (
+    <div className="App">
+      <div>
+        <h2>Hello</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
