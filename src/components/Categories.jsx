79ba61df9b3328c2136d8be900c9bac58eeea0a7
romantut1988 <romantut1988@mail.ru>
2022-09-14 11:06:34 +0300

    product features via usestate

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,16 +1,22 @@
-function Categories() {
-    return (
-      <div className="categories">
-        <ul>
-          <li className="active">Все</li>
-          <li>Мясные</li>
-          <li>Вегетарианская</li>
-          <li>Гриль</li>
-          <li>Острые</li>
-          <li>Закрытые</li>
-        </ul>
-      </div>
-    )
-  }
+import React, { useState } from "react";
 
-  export default Categories;
+function Categories() {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const categories = ['Все','Мясные','Вегетарианская','Гриль','Острые','Закрытые'];
+
+  return (
+    <div className="categories">
+      <ul>
+        {
+          categories.map((value, index) => (
+            <li onClick={() => setActiveIndex(index)} className={activeIndex === index ? 'active' : ''}>
+              {value}
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Categories;
