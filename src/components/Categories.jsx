be30bceb1fff58a850838a43fcc5cbb26b08aca3
romantut1988 <romantut1988@mail.ru>
2022-09-17 01:13:23 +0300

    functionality for sorting and filtering pizzas

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,20 +1,18 @@
 import React, { useState } from "react";
 
-function Categories() {
-  const [activeIndex, setActiveIndex] = useState(0);
-
+function Categories({value, onChangeCategory}) {
   const categories = ['Все','Мясные','Вегетарианская','Гриль','Острые','Закрытые'];
 
   return (
     <div className="categories">
       <ul>
         {
-          categories.map((value, index) => ( 
+          categories.map((categoryName, index) => ( 
             <li
              key={index}
-              onClick={() => setActiveIndex(index)}
-              className={activeIndex === index ? 'active' : ''}>
-              {value}
+              onClick={() => onChangeCategory(index)}
+              className={value === index ? 'active' : ''}>
+              {categoryName}
             </li>
           ))}
       </ul>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,22 +8,38 @@ import Sceleton from "../components/PizzaBlock/Sceleton";
 const Home = () => {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [categoryId, setCategoryId] = useState(0);
+  const [sortType, setSortType] = useState({
+    name: "популярности",
+    sortProperty: "rating",
+  });
 
   useEffect(() => {
-    fetch("https://6321bcd7fd698dfa29fde888.mockapi.io/items")
+    setIsLoading(true);
+
+    const sortBy = sortType.sortProperty.replace("-", "");
+    const order = sortType.sortProperty.includes("-") ? "asc" : "desc";
+    const category = categoryId > 0 ? `category=${categoryId}` : "";
+
+    fetch(
+      `https://6321bcd7fd698dfa29fde888.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`, 
+    )
       .then((res) => res.json())
       .then((arr) => {
         setItems(arr);
         setIsLoading(false);
       });
-      window.scrollTo(0, 0);
-  }, []);
+    window.scrollTo(0, 0);
+  }, [categoryId, sortType]);
 
   return (
     <div className="container">
       <div className="content__top">
-        <Categories />
-        <Sort />
+        <Categories
+          value={categoryId}
+          onChangeCategory={(index) => setCategoryId(index)}
+        />
+        <Sort value={sortType} onChangeSort={(index) => setSortType(index)} />
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
