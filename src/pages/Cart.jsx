f6c89b1f236312f669563f2d63a3f8b280e68cd7
romantut1988 <romantut1988@mail.ru>
2022-09-19 11:31:12 +0300

    console error fixes

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -404,9 +404,9 @@ const Cart = () => {
                 <path
                   d="M7 13L1 6.93015L6.86175 1"
                   stroke="#D3D3D3"
-                  stroke-width="1.5"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 />
               </svg>
 
