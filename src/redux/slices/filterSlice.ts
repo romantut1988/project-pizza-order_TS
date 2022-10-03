8e77206cd4f58d0dc63d106b799200f4cf46eb51
romantut1988 <romantut1988@mail.ru>
2022-10-03 14:02:02 +0300

    fixed redux sorting

---
+++ src/redux/slices/filterSlice.ts
@@ -0,0 +1,71 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { type } from "os";
+import { RootState } from "../store";
+
+type Sort = {
+  name: string;
+  sortProperty: "rating";
+};
+
+interface FilterSliceState {
+  searchValue: string;
+  categoryId: number;
+  currentPage: number;
+  sort: Sort;
+}
+
+const initialState: FilterSliceState = {
+  searchValue: "",
+  categoryId: 0,
+  currentPage: 1,
+  sort: {
+    name: "популярности",
+    sortProperty: "rating",
+  },
+};
+
+const filterSlice = createSlice({
+  name: "filters",
+  initialState,
+  reducers: {
+    setCategoryId(state, action) {
+      state.categoryId = action.payload;
+    },
+    setSearchValue(state, action) {
+      state.searchValue = action.payload;
+    },
+    setSort(state, action) {
+      state.sort = action.payload;
+    },
+    setCurrentPage(state, action) {
+      state.currentPage = action.payload;
+    },
+    setFilters(state, action) {
+      if (Object.keys(action.payload).length) {
+        state.currentPage = Number(action.payload.currentPage);
+        state.categoryId = Number(action.payload.categoryId);
+        state.sort = action.payload.sort;
+      } else {
+        state.currentPage = 1;
+        state.categoryId = 0;
+        state.sort = {
+          name: "Популярности",
+          sortProperty: "rating",
+        };
+      }
+    },
+  },
+});
+
+export const selectFilter = (state: RootState) => state.filter;
+export const selectSort = (state: RootState) => state.filter.sort;
+
+export const {
+  setCategoryId,
+  setSort,
+  setCurrentPage,
+  setFilters,
+  setSearchValue,
+} = filterSlice.actions;
+
+export default filterSlice.reducer;
